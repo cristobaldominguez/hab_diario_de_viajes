0aab69e65c5cd03b6fb685c6e927bb6c594ca518
Cristobal Dominguez <cristobald@gmail.com>
2023-08-09 11:12:34 +0200

    Adds a db MySQL Connector

--- server/config.js
+++ server/config.js
@@ -3,6 +3,17 @@ import 'dotenv/config'
 
 const port = process.env.PORT || 3000
 
+const {
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE
+} = process.env
+
 export {
-  port
+  port,
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE
 }
